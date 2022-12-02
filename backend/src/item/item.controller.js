75fcf0031ae0aad66b81c6c377405154e0b56d0f
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2022-12-01 20:50:45 -0400

    Criação do controller Create, Update e DeleteById

--- backend/src/item/item.controller.js
+++ backend/src/item/item.controller.js
@@ -1,7 +1,77 @@
+const { isObjectIdValid } = require("../db/database.helper");
+
+// Apresenta todos os dados dos itens
 const findAll = (req, res) => {
-  res.send("Find All");
+  const items = [];
+  res.send(items);
 };
 
+// Busca os dados por ID
+const findById = (req, res) => {
+  const id = req.params.id;
+
+  //   Validação de id
+  if (!isObjectIdValid(id)) {
+    return res.status(404).send({ message: "ID inválido" });
+  }
+  const item = {};
+  //   Validação se o item foi encontrado
+  if (!item) {
+    return res.status(404).send({ message: "Item não encontrado." });
+  }
+  res.send(item);
+};
+
+// Cria um novo item
+const create = (req, res) => {
+  const item = req.body;
+
+  if (!item || !item.name || !item.imageUrl || !item.category) {
+    return res.status(404).send({ message: "Dados inválidos." });
+  }
+  const newItem = {};
+
+  res.status(201).send(newItem);
+};
+
+// Realiza atualização do item
+const update = (req, res) => {
+  if (!updateItem) {
+    const id = req.params.id;
+
+    if (!isObjectIdValid(id)) {
+      return res.status(404).send({ message: "ID inválido" });
+    }
+    const item = req.body;
+
+    if (!item || !item.name || !item.imageUrl || !item.category) {
+      return res.status(404).send({ message: "Dados inválidos." });
+    }
+  }
+  res.send({ message: "Item atualizado com sucesso!" });
+};
+
+// Deleta um item especifico
+const deleteById = (req, res) => {
+  const id = req.params.id;
+
+  if (!isObjectIdValid(id)) {
+    return res.status(404).send({ message: "ID inválido" });
+  }
+  const deletedItem = {};
+
+  if (!deletedItem) {
+    return res.status(404).send({ message: "Item não encontrado!" });
+  }
+
+  res.send({ message: "Item excluido com sucesso!" });
+};
+
+// Todas as funções são exportadas
 module.exports = {
   findAll,
+  findById,
+  create,
+  update,
+  deleteById,
 };
--- backend/src/item/item.router.js
+++ backend/src/item/item.router.js
@@ -7,5 +7,9 @@ const router = express.Router();
 
 // ROTAS
 router.get("/", controller.findAll);
+router.get("/:id", controller.findById);
+router.post("/", controller.create);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.deleteById);
 
 module.exports = router;
